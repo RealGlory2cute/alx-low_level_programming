4b2f4060fbaf38ff09787df43946083a5e088411
RealGlory2cute <glory2cute@yahoo.com>
2022-09-10 01:32:17 -0700

    Task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 
-
 /**
  * main = Print in ascending order, seperated by a comma followed by a space.
  *
@@ -10,28 +9,24 @@ int main(void)
 {
 	int digit1, digit2;
 
-
 	for (digit1 = 0; digit1 < 9; digit1++)
 	{
+
 		for (digit2 = digit1 + 1; digit2 < 10; digit2++)
 		{
 
 			putchar((digit1 % 10) + '0');
 			putchar((digit2 % 10) + '0');
 
-
 			if (digit1 == 8 && digit2 == 9)
-				continue;
-			
+				continue;			
 
 			putchar(',');
 			putchar(' ');
 		}
 	}
 
-
 	putchar('\n');
-
 
 	return (0);
 }
