744d4e6231fcc8bfc99dd0dda97107d2550a740d
RealGlory2cute <glory2cute@yahoo.com>
2022-09-13 17:45:09 -0700

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,6 @@
 #include "main.h"
-
 /**
- * main - Entry point
+ * main - Entry - point
  *
  * Return: Always 0 (Success)
  */
@@ -17,4 +16,4 @@ int main(void)
 	_putchar('\n');
 
 	return (0);
-}	
+}
