103bcfc58b90b1aa30ca868dd175562f2e258be7
RealGlory2cute <glory2cute@yahoo.com>
2022-09-29 15:38:23 -0700

    Task 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -6,6 +6,7 @@ int _sqrt(int n, int i);
 /**
  * _sqrt_recursion - Returns the natural square root of a number
  * @n: number to calculate the natural square root
+ * @i: iterate number
  *
  * Return: the natural square root
  */
