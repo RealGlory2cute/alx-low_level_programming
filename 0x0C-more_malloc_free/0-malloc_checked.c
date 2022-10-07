d456e07a71864d59638f8d3ead517223879137e3
RealGlory2cute <glory2cute@yahoo.com>
2022-10-06 19:34:06 -0700

    Task 0

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -7,7 +7,6 @@
  * Return: pointer to the allocated memory.
  * if malloc fails, status value is equal to 98.
  */
-
 void *malloc_checked(unsigned int b)
 {
 	char *p;
@@ -15,6 +14,5 @@ void *malloc_checked(unsigned int b)
 	p = malloc(b);
 	if(p == NULL)
 		exit(98);
-
 	return (p);
 }
