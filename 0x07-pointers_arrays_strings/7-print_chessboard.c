9b9f627b82370b3cced4fdc924b3c7da5a166df6
RealGlory2cute <glory2cute@yahoo.com>
2022-10-12 03:24:52 -0700

    Task 6

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -3,7 +3,7 @@
 /**
  * print_chessboard - prints the chess board
  *
- * @: input pointer
+ * @a: input pointer
  *
  * Return: return nothing
  */
@@ -19,7 +19,7 @@ void print_chessboard(char (*a)[8])
 		if (i % 8 == 0 && i != 0)
 		{
 			j = i;
-			_putchar('\n);
+			_putchar('\n');
 		}
 
 		_putchar(a[i / 8][i - j]);
